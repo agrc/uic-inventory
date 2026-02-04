5ee5e37edbc86ae0a870849e521c37118dd0e670
steveoh <sgourley@utah.gov>
2026-02-03 19:16:14 -0700

    chore: add login check and note with issue around watch

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -5,10 +5,18 @@ docker-compose down
 echo "starting databases"
 docker-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
 
-echo "authenticating with GCP"
-gcloud auth application-default login
+# Check if GCP credentials are valid
+if ! gcloud auth application-default print-access-token &>/dev/null; then
+  echo "authenticating with GCP"
+  gcloud auth application-default login
+fi
 
 echo "starting api"
 cd src/api
+
+# Start API with watch
+export ASPNETCORE_ENVIRONMENT=Development
+# there is a bug with this currently. must use launch task and dotnet run
+# https://github.com/dotnet/aspnetcore/issues/64519
 export ASPNETCORE_HOSTINGSTARTUPASSEMBLIES=Microsoft.AspNetCore.SpaProxy
 dotnet watch
